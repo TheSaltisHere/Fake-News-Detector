ae343869e47cf91f030e56433174441917ee02d0
Rishab Chakrabarti <rishab.chakrabarti@gmail.com>
2021-08-17 14:26:27 +0530

    final

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas as pd
 import itertools
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import PassiveAggressiveClassifier
-from sklearn.feature_extraction.text import TdifVectorizer
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 # Read the data
